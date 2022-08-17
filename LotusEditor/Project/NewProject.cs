cf7e3daf7273b89e7617aa5488de486b7f083c42
TigerCipher <matt.rogers8396@gmail.com>
2022-08-16 23:03:01 -0500

    Project name and path validation

--- LotusEditor/Project/NewProject.cs
+++ LotusEditor/Project/NewProject.cs
@@ -41,6 +41,10 @@ namespace LotusEditor.Project
 
         public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
 
+        private bool _isValid;
+
+        private string _errorMessage;
+
         public NewProject()
         {
             ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
@@ -60,12 +64,60 @@ namespace LotusEditor.Project
 
                     _projectTemplates.Add(template);
                 }
+                ValidateProjectPath();
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
                 // #TODO: Log console msgs
             }
+        }
+
+        private bool ValidateProjectPath()
+        {
+            var path = ProjectPath;
+            if (!Path.EndsInDirectorySeparator(path))
+            {
+                path += @"\";
+            }
+
+            path += $@"{ProjectName}\";
+
+            IsValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProjectName.Trim()))
+            {
+                ErrorMessage = "You must supply a project name";
+                IsValid = false;
+            }
+
+            if (ProjectName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
+            {
+                ErrorMessage = "Invalid character(s) in project name!";
+                IsValid = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ProjectPath.Trim()))
+            {
+                ErrorMessage = "Select a valid project path";
+                IsValid = false;
+            }
+
+            if (ProjectPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                ErrorMessage = "Invalid character(s) in project path!";
+                IsValid = false;
+            }
+
+            if (Directory.Exists(path) && Directory.EnumerateFileSystemEntries(path).Any())
+            {
+                ErrorMessage = "The provided path already exists and is not empty";
+                IsValid = false;
+            }
+
+            if (IsValid) ErrorMessage = string.Empty;
+
+            return IsValid;
         }
 
         public string ProjectName
@@ -76,6 +128,7 @@ namespace LotusEditor.Project
                 if (_name != value)
                 {
                     _name = value;
+                    ValidateProjectPath();
                     OnPropertyChanged(nameof(ProjectName));
                 }
             }
@@ -89,11 +142,37 @@ namespace LotusEditor.Project
                 if (_path != value)
                 {
                     _path = value;
+                    ValidateProjectPath();
                     OnPropertyChanged(nameof(ProjectPath));
                 }
             }
         }
 
+        public bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged(nameof(IsValid));
+                }
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                if (_errorMessage != value)
+                {
+                    _errorMessage = value;
+                    OnPropertyChanged(nameof(ErrorMessage));
+                }
+            }
+        }
 
     }
 
