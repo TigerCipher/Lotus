d436d4c05259be026002f6e39df2bad635f1df8b
TigerCipher <matt.rogers8396@gmail.com>
2022-08-19 18:38:09 -0500

    removed lua file and added guid to pch header guard

--- Lotus/src/pch.h
+++ Lotus/src/pch.h
@@ -1,27 +1,27 @@
 // ------------------------------------------------------------------------------
-// 
+//
 // Lotus
 //    Copyright 2022 Matthew Rogers
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-// 
+//
 // File Name: pch.h
 // Date File Created: 08/19/2022
 // Author: Matt
-// 
+//
 // ------------------------------------------------------------------------------
-#ifndef PCH_H
-#define PCH_H
+#ifndef LOTUS_PCH_H_7_B5468_E5_CDCF445_BB_08DE75_F_3376D_4D3
+#define LOTUS_PCH_H_7_B5468_E5_CDCF445_BB_08DE75_F_3376D_4D3
 
 #include <cstdint>
 #include <cassert>
@@ -38,4 +38,4 @@
 
 #include "lotus/common/Common.h"
 
-#endif //PCH_H
+#endif //LOTUS_PCH_H_7_B5468_E5_CDCF445_BB_08DE75_F_3376D_4D3
