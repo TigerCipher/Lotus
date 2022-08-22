16b6a6844060e23efb1c618eddaa29e725b69fc8
TigerCipher <matt.rogers8396@gmail.com>
2022-08-22 17:07:52 -0500

    Created some custom drag number/vector controls

--- LotusEditor/Dictionaries/ControlTemplates.xaml.cs
+++ LotusEditor/Dictionaries/ControlTemplates.xaml.cs
@@ -54,5 +54,42 @@ namespace LotusEditor.Dictionaries
             var win = (Window)((FrameworkElement)sender).TemplatedParent;
             win.WindowState = WindowState.Minimized;
         }
+
+        private void TextBoxRename_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp == null) return;
+
+            if (e.Key == Key.Enter)
+            {
+                if (textBox.Tag is ICommand command && command.CanExecute(textBox.Text))
+                {
+                    command.Execute(textBox.Text);
+                }
+                else
+                {
+                    exp.UpdateSource();
+                }
+
+                textBox.Visibility = Visibility.Collapsed;
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                exp.UpdateTarget();
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void TextBoxRename_OnFocusLost(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp == null) return;
+            exp.UpdateTarget();
+            textBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Previous));
+            textBox.Visibility = Visibility.Collapsed;
+        }
     }
 }
---
+++ LotusEditor/Utility/Controls/NumberBox.cs
@@ -0,0 +1,111 @@
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LotusEditor.Utility.Controls
+{
+    [TemplatePart(Name = "PART_textBlock", Type = typeof(TextBlock))]
+    [TemplatePart(Name = "PART_textBox", Type = typeof(TextBox))]
+    internal class NumberBox : Control
+    {
+        private double _originalValue;
+        private bool _capturedMouse = false;
+        private bool _valueChanged = false;
+        private double _mouseXStart;
+        private double _speedMultiplier;
+
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value), typeof(string), typeof(NumberBox), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly DependencyProperty MultiplierProperty = DependencyProperty.Register(
+            nameof(Multiplier), typeof(double), typeof(NumberBox), new PropertyMetadata(1.0));
+
+        public double Multiplier
+        {
+            get => (double)GetValue(MultiplierProperty);
+            set => SetValue(MultiplierProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register(
+    nameof(Label), typeof(string), typeof(NumberBox), new PropertyMetadata());
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        static NumberBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NumberBox), new FrameworkPropertyMetadata(typeof(NumberBox)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (GetTemplateChild("PART_textBlock") is not TextBlock textBlock) return;
+
+            if (!string.IsNullOrEmpty(Label)) textBlock.Margin = new Thickness(10, 0, 0, 0);
+            textBlock.MouseLeftButtonDown += TextBlock_OnMouseLeftDown;
+            textBlock.MouseLeftButtonUp += TextBlock_OnMouseLeftUp;
+            textBlock.MouseMove += TextBlock_OnMouseMove;
+        }
+
+        private void TextBlock_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_capturedMouse) return;
+
+            var mouseX = e.GetPosition(this).X;
+            var delta = mouseX - _mouseXStart;
+            if (Math.Abs(delta) > SystemParameters.MinimumHorizontalDragDistance)
+            {
+                if (Keyboard.Modifiers.HasFlag(ModifierKeys.Control)) _speedMultiplier = 0.001;
+                else if (Keyboard.Modifiers.HasFlag(ModifierKeys.Shift)) _speedMultiplier = 1;
+                else _speedMultiplier = 0.1;
+                var newValue = _originalValue + (delta * _speedMultiplier * Multiplier);
+                Value = newValue.ToString("0.#####");
+                _valueChanged = true;
+            }
+        }
+
+        private void TextBlock_OnMouseLeftUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_capturedMouse)
+            {
+                Mouse.Capture(null);
+                _capturedMouse = false;
+                e.Handled = true;
+                if (!_valueChanged && GetTemplateChild("PART_textBox") is TextBox textBox)
+                {
+                    textBox.Visibility = Visibility.Visible;
+                    textBox.Focus();
+                    textBox.SelectAll();
+                }
+            }
+        }
+
+        private void TextBlock_OnMouseLeftDown(object sender, MouseButtonEventArgs e)
+        {
+            double.TryParse(Value, out _originalValue);
+            Mouse.Capture(sender as UIElement);
+            _capturedMouse = true;
+            _valueChanged = false;
+            e.Handled = true;
+            _mouseXStart = e.GetPosition(this).X;
+        }
+    }
+}
