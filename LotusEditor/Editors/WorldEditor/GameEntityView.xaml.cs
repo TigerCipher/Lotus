0f7d10d62342cf8596555d098eade8abf403f154
TigerCipher <matt.rogers8396@gmail.com>
2022-08-26 10:44:27 -0500

    fixed native debugging capabilities and disabled editing components when the entity is disabled

--- LotusEditor/Editors/WorldEditor/GameEntityView.xaml.cs
+++ LotusEditor/Editors/WorldEditor/GameEntityView.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,19 @@ using LotusEditor.Utility;
 
 namespace LotusEditor.Editors
 {
+
+    public class NullableBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is true;
+        }
+    }
     /// <summary>
     /// Interaction logic for GameEntityView.xaml
     /// </summary>
